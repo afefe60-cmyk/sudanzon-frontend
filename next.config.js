363df6eb26231e866a5dd91bd1f44b88056c3926
zain <afefe60@gmail.com>
2026-09-12 18:00:25 +0200

    fix: simplify next.config.js and remove custom header matchers

--- next.config.js
+++ next.config.js
@@ -16,31 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
-  },
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|jpeg|png|webp|avif|gif|ico|woff|woff2|ttf)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
